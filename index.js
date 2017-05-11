cdbf849eab2671d36e9a8a41934c8b3f23f3f411
deepHash <itsesisx@gmail.com>
2017-05-11 16:51:31 +0300

    exercise1.0 release

--- index.js
+++ index.js
@@ -8,24 +8,26 @@ const express = require ('express'),
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+//default page to for API instructions
 app.get('/', (req,res) => {
     //send API
     res.sendFile(`${__dirname}/api.html`);
 });
 
+//get route
 app.get('/getAllPayments/', (req,res) => {
     res.status(200).json(data.getAllPayments());
 });
-
+//post route
 app.post('/getUserData/', (req,res) => {
     res.status(200).json(data.getUserData(req.body.id));
 });
-
+//put route
 app.put('/getTypeByPrice/:min_price/:max_price', (req,res) => {
     res.status(200).json(data.getTypeByPrice(
                                 req.params.min_price,req.params.max_price));
 });
-
+//global handler
 app.all('*', (req, res) => {
     res.send(`error: route not found, global handler`);
 });
